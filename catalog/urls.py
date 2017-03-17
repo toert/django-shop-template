058fa4768218d48a79838b2f05a9fa43d8e7790d
toert <pw177@mail.ru>
2017-03-17 09:59:12 +0300

    add first views with urls patterns

--- catalog/urls.py
+++ catalog/urls.py
@@ -19,4 +19,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^P<category_slug>/$', views.list_products, name='list_products_by_category')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,19 @@
-#from django.shortcuts import render, get_object_or_404
-#from .models import Category, Product
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
 from django.shortcuts import HttpResponse
 
-def index(request, category_slug=None):
+def index(request):
     return HttpResponse("Hello, world. You're at the catalog index.")
+
+def list_products(request, category_slug=None):
+    print(request)
+    if category_slug:
+        current_category = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=current_category)
+    else:
+        current_category = None
+        products = Product.objects.all()
+    return render(request, 'templates/catalog/list.html', {
+                  'category': current_category,
+                  'products': products
+    })
